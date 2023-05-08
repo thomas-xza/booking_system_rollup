e0a515c33a16c076a38c56b50cee634ac5885d66
thomas-xza <thomas@xza.fr>
2023-05-08 16:09:16 +0100

    At the point where calender API call would need to happen but admittedly have lost interest

--- src/Mapbox.js
+++ src/Mapbox.js
@@ -2,35 +2,56 @@ import React from 'react';
 import destinations from './locations.json';
 
 export default function Mapbox({ postcode, mapbox_res, set_mapbox_res }) {
-
     
-    if (mapbox_res.longitude) {
-	
-	return (
-	    JSON.stringify(mapbox_res)
-	)
-	
-    } else {
+    if (mapbox_res === {}) {
 	
 	load_distances(postcode, mapbox_res, set_mapbox_res);
 
 	return (
-		<div className="loading">Calling Mapbox API</div>
+	    <div className="loading">Loading Mapbox API</div>
+	)
+
+    } else if (mapbox_res.longitude) {
+	
+	return (
+	    JSON.stringify(mapbox_res)
+	)
+
+    } else {
+
+	return (
+	    <div>An error occured whilst looking up the postcode.</div>
 	)
 
     };
 
 };
 
-export async function load_distances(postcode, mapbox_res, set_mapbox_res) {
+async function load_distances(postcode, mapbox_res, set_mapbox_res) {
 
     const url = postcode_to_url(postcode);
 
     const mapbox_json = await fetch(url)
 	  .then(res => res.json())
 
-    mapbox_json.resolve(store_data(postcode, mapbox_json, mapbox_res, set_mapbox_res));
+    store_data(postcode, mapbox_json, mapbox_res, set_mapbox_res);
     
+};
+
+function postcode_to_url(postcode) {
+
+    //  Note: limit as of 2023-04 is 600 geocode requests/minute
+    //  https://docs.mapbox.com/api/overview/
+
+    //  'AAA AAA'
+    //  -->  'https://api.mapbox.com/...AAA AAA...'
+
+    const mapbox_api_key = 'pk.eyJ1IjoidGVzdC14emEiLCJhIjoiY2xnOXNiaWRvMWFtcDNlcWxzZjRmZDdldSJ9.lKZ_CDvdIh1Y9dw2DGjbOA';
+
+    return 'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
+        postcode + ', United Kingdom.json?access_token=' +
+        mapbox_api_key;
+
 };
 
 function store_data(postcode, mapbox_json, mapbox_res, set_mapbox_res) {
@@ -49,35 +70,6 @@ function store_data(postcode, mapbox_json, mapbox_res, set_mapbox_res) {
     console.log(source_obj);
 
     set_mapbox_res(source_obj);
-
-};
-
-async function fetch_data(location_obj, index) {
-
-    //  { postcode: 'AAA AAA', ... }
-    //  -->  { postcode: 'AAA AAA', ...,
-    //        long: '0.002020',
-    //        lat: '-0.304373' }
-
-    const url = postcode_to_url(location_obj['postcode']);
-
-    return location_obj;
-
-};
-
-export function postcode_to_url(postcode) {
-
-    //  Note: limit as of 2023-04 is 600 geocode requests/minute
-    //  https://docs.mapbox.com/api/overview/
-
-    //  'AAA AAA'
-    //  -->  'https://api.mapbox.com/...AAA AAA...'
-
-    const mapbox_api_key = 'pk.eyJ1IjoidGVzdC14emEiLCJhIjoiY2xnOXNiaWRvMWFtcDNlcWxzZjRmZDdldSJ9.lKZ_CDvdIh1Y9dw2DGjbOA';
-
-    return 'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
-        postcode + ', United Kingdom.json?access_token=' +
-        mapbox_api_key;
 
 };
 
