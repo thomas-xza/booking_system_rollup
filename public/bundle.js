04f164c9fdff2d955c3bd534bad8238fc8f67967
mslockin <133052424+mslockin@users.noreply.github.com>
2023-05-19 11:37:02 +0000

    Added a JSON of clinics, using an online CSV-to-JSON tool

--- public/bundle.js
+++ public/bundle.js
@@ -8031,26 +8031,21 @@
 	    load_distances(postcode, mapbox_res, set_mapbox_res);
 	    return /*#__PURE__*/React.createElement("div", {
 	      className: "loading"
-	    }, "Calling Mapbox API");
+	    }, "Loading Mapbox API");
 	  }
 	}
 	async function load_distances(postcode, mapbox_res, set_mapbox_res) {
 	  const url = postcode_to_url(postcode);
-	  const mapbox_json = await fetch(url).then(res => res.json());
-	  mapbox_json.resolve(store_data(postcode, mapbox_json, mapbox_res, set_mapbox_res));
+	  const mapbox_json = await fetch_with_retries(url);
+	  store_data(postcode, mapbox_json, mapbox_res, set_mapbox_res);
 	}
-	function store_data(postcode, mapbox_json, mapbox_res, set_mapbox_res) {
-	  console.log("store_data()", mapbox_json, "\n", mapbox_json.features[0]);
-	  const top_hit_coords = mapbox_json.features[0].geometry.coordinates;
-	  const source_obj = {
-	    'postcode': postcode,
-	    'longitude': top_hit_coords[0],
-	    'latitude': top_hit_coords[1]
-	    // 'borough': mapbox_json.features[0].context[0].text || ""
-	  };
-
-	  console.log(source_obj);
-	  set_mapbox_res(source_obj);
+	async function fetch_with_retries(url, retry_count) {
+	  try {
+	    return await fetch(url).then(res => res.json());
+	  } catch (error) {
+	    console.log("retrying fetch(), retry_count");
+	    return fetch_with_retries(url);
+	  }
 	}
 	function postcode_to_url(postcode) {
 	  //  Note: limit as of 2023-04 is 600 geocode requests/minute
@@ -8061,6 +8056,21 @@
 
 	  const mapbox_api_key = 'pk.eyJ1IjoidGVzdC14emEiLCJhIjoiY2xnOXNiaWRvMWFtcDNlcWxzZjRmZDdldSJ9.lKZ_CDvdIh1Y9dw2DGjbOA';
 	  return 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + postcode + ', United Kingdom.json?access_token=' + mapbox_api_key;
+	}
+	function store_data(postcode, mapbox_json, mapbox_res, set_mapbox_res) {
+	  console.log("store_data()", mapbox_json, "\n", mapbox_json.features[0]);
+	  try {
+	    const top_hit_coords = mapbox_json.features[0].geometry.coordinates;
+	    const source_obj = {
+	      'postcode': postcode,
+	      'longitude': top_hit_coords[0],
+	      'latitude': top_hit_coords[1]
+	      // 'borough': mapbox_json.features[0].context[0].text || ""
+	    };
+
+	    console.log(source_obj);
+	    set_mapbox_res(source_obj);
+	  } catch {}
 	}
 
 	function Booking({
