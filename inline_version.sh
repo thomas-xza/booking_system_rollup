faf2e1dfd93315806a52e351201f5ba0ba35c680
mslockin <133052424+mslockin@users.noreply.github.com>
2023-05-28 13:00:40 +0100

    Fixed up inline_version.sh to embed CSS

--- inline_version.sh
+++ inline_version.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-cat public/index_inline_skel_head.html	>  public/booking_system_full.html
+cat public/index_inline_skel_head.html	        >  public/booking_system_full.html
 
-cat public/bundle.js			>> public/booking_system_full.html
+cat public/css/normalize.css                    >>  public/booking_system_full.html
 
-cat public/index_inline_skel_tail.html	>> public/booking_system_full.html
+cat public/css/stylize.css                      >>  public/booking_system_full.html
+
+cat public/index_inline_skel_head_post_css.html	>>  public/booking_system_full.html
+
+cat public/bundle.js			        >> public/booking_system_full.html
+
+cat public/index_inline_skel_tail.html	        >> public/booking_system_full.html
