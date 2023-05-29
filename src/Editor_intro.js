4d78db4403c5dbdb6d5f545e10e33405ff3704e0
mslockin <133052424+mslockin@users.noreply.github.com>
2023-05-29 13:32:33 +0100

    Minor tweaks to explanation of editor. Done for today.

--- src/Editor_intro.js
+++ src/Editor_intro.js
@@ -10,7 +10,7 @@ export default function Editor_intro () {
 
 	This page is about editing the set of clinics which will be loaded when making a booking. <br/>
 
-	Basically the formatting of the following textbox (which contains data in Javascript format) has to be what the Javascript engine in your brower can understand. This page was created so that you can edit the data and get immediate feedback (at bottom of page) if your edits are not compatible with this booking system. <br/><br/>
+	Basically the formatting of the following textbox (which contains data in Javascript format) has to be what the Javascript engine in your brower can understand. This page was created so that you can edit the data and get immediate feedback (at bottom of page) in case your edits are not compatible with this booking system. <br/><br/>
 
 	The following expectations should be known, before starting:
 	    <ul>
@@ -20,7 +20,7 @@ export default function Editor_intro () {
 	    <li><code>postcode</code> must always be uppercase and be a standard, full UK postcode</li>
 	    <li><code>address</code> must be a set of quotes, separated by commas, within square brackets (imitate the pattern of the default list)</li>
 	    <li><code>longitude</code> and <code>latitude</code> must always be numbers between -1 and 52</li>
-	    <li>telephone clinics must have <code>longitude</code> set to <code>0</code> and do not need to have valid <code>postcode</code>, but do need to have <code>longitude</code> set to 0</li>
+	    <li>telephone clinics <strong>must</strong> have <code>longitude</code> set to <code>0</code> but do not need to have valid <code>postcode</code> (all co-ordinates are used to calculate distances, without exception, so omitted co-ordinates will cause the booking selection to crash, and there are also various checks for <code>0</code> elsewhere...)</li>
 	    </ul>
 
 	    <strong>Note that once you close your browser any edits will not be saved, so you need to make a copy and store it elsewhere, for laster pasting back in.</strong> <br/><br/>
