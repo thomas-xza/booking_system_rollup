f8b54a52dfb1f68f6b3bf1d004d3fa674685c5ef
mslockin <133052424+mslockin@users.noreply.github.com>
2023-06-07 09:43:07 +0000

    Minor styling edits

--- public/bundle.js
+++ public/bundle.js
@@ -8191,7 +8191,7 @@
 	    onClick: () => {
 	      set_page_flow(5);
 	    }
-	  }, "To clinic editor")), /*#__PURE__*/React.createElement("h1", null, "Client/patient entry"), /*#__PURE__*/React.createElement("label", null, /*#__PURE__*/React.createElement("em", null, "Optional"), " paste a row from spreadsheet:"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("input", {
+	  }, "To clinic editor")), /*#__PURE__*/React.createElement("h1", null, "Client data entry"), /*#__PURE__*/React.createElement("label", null, /*#__PURE__*/React.createElement("em", null, "Optional"), " paste a row from spreadsheet:"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("input", {
 	    value: form_data.paste,
 	    onChange: handle_paste
 	  }), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("label", null, "Name:"), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("input", {
@@ -8226,7 +8226,7 @@
 	    //     JSON.stringify(mapbox_resp)
 	    // )
 
-	    return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("em", null, postcode, " is located in ", mapbox_resp.borough), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("strong", null, "Select client/patient availability"), ":");
+	    return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("em", null, postcode, " is located in ", mapbox_resp.borough), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("br", null), /*#__PURE__*/React.createElement("strong", null, "Select client availability"), ":");
 	  } else {
 	    load_distances(postcode, mapbox_resp, set_mapbox_resp);
 	    return /*#__PURE__*/React.createElement("div", {
@@ -8856,14 +8856,12 @@
 <a href="https://digital.nhs.uk/developer/api-catalogue/nhsmail" target="_blank">Exchange API</a> which is <a href="https://github.com/OfficeDev/ews-managed-api" target="_blank">almost deprecated</a>) or if all LSSS staff move to <a href="https://developers.google.com/calendar/api/guides/overview" target="_blank">Google calendar</a> until then</li>
 <li>It would also be possible to actually show only available appointments and select from them if something could be done about the calendar</li>
 <li>"Send text via Vonage" will only work if someone gives me a credit card to <a href="https://www.vonage.co.uk/communications-apis/sms/pricing/">pay the fees</a></li>
-<li>If the clinics change there is not *yet* a user-friendly way to update them on this website</li>
 </ul>
 <br/>
 
 <h3>Potential improvements:</h3>
 <ul>
 <li>After entering someone's details the 1st time, it could be stored within the browser (localStorage) on the computer, making selection possible, for the same person, next time</li>
-<li>It should be possible to change the set of clinics (currently hardcoded) early on, such as by uploading a user-friendly file (.csv) that can be edited in Excel.</li>
 </ul>
 <br/><br/><br/><br/><br/>
 `;
